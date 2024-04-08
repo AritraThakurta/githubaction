48fa7a1de5e3176942af0cd1784228eee5cbe9a4
aritrathakurta <aritra.thakurta89@gmail.com>
2024-04-08 18:14:50 +0530

    code

---
+++ src/main/java/com/example/springgithubactionscicd/SpringGithubactionsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgithubactionscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubactionsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubactionsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springgithubactionscicd/SpringGithubactionsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgithubactionscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubactionsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
